d5dcdb10bffae484cca538030bc2233c95b33264
Abhisek <g38099933@gmail.com>
2026-05-03 20:33:07 +0545

    py test update for cicd

--- python-backend/tests/test_placeholder.py
+++ python-backend/tests/test_placeholder.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import pytest
 from app import app
 from chatbot import predict_disease, get_recommendations
